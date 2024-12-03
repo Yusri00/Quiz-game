ed04721b5374330f70e5136acb24cd21c1c4baff
Yusra Aman <Yusra.aman00@gmail.com>
2024-12-03 23:27:55 +0100

    Shows first question and options

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let questions = []; 
+
 async function fetchData(){
 
     try{
@@ -5,14 +7,31 @@ async function fetchData(){
         if(!response.ok){
             throw new Error(`Http error! status: ${response.status}`);
         }
-        const data = await response.json();
-        const dataDisplay = document.getElementById('dataDisplay');
+        questions = await response.json();
+        showQuestion();
 
-        // Visa data i HTML
-        dataDisplay.textContent = JSON.stringify(data, null, 2);
+        // To show data in HTML         
+        const dataDisplay = document.getElementById('dataDisplay');
+        dataDisplay.textContent = JSON.stringify(questions, null, 2);
     }
     catch(error){
         console.error('API could not be read', error);
     }
 }
 fetchData();
+
+const root = document.getElementById("root");
+let currentQuestionIndex = 0; // Track the current question
+let score = 0; // Track the user's score
+let timer; // Store the timer so it can be cleared
+
+
+function showQuestion (){
+    const currentQuestion = questions[currentQuestionIndex];
+    console.log(currentQuestion); 
+    const options = currentQuestion.options;
+    console.log(options[0]);
+    dataDisplay.innerHTML = "";
+
+}
+
